3c604d9d48f79bc60129cb70637a68bd5394deaf
benkores <benkores@gmail.com>
2022-12-06 10:35:46 -0500

    update navbar and db connection for boarding pass

--- boarding_passes.php
+++ boarding_passes.php
@@ -1,35 +1,55 @@
+<?php
+session_start();
+require_once 'config/connect.php';
+if (isset($_SESSION['AccountID'])) {
+  $_SESSION['login'] = "Logout";
+} else {
+  $_SESSION['login'] = "Login";
+}
+?>
 <!doctype html>
 <html lang="en">
   <head>
     <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon" href="img/favicon.ico">
+  <!-- Bootstrap CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <link rel="stylesheet" href="css/styles.css">
 
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/styles.css">
+  <title>Home</title>
+</head>
 
-    <title>Boarding Passes</title>
-  </head>
-  <body>
-    <div class="header">
+<body>
+  <div class="header">
+    <nav class="navbar navbar-default">
       <img src="/img/airplane_header.PNG" alt="Airplane Header">
-
-      <nav class="navbar navbar-default">
-        <div class="container-fluid">
-          <div class="navbar-header">
-            <a class="navbar-brand" href="/#"><b>BAX Airlines</b></a>
-          </div>
-          <ul class="nav navbar-nav">
-            <li><a href="/book.php">Book a Flight</a></li>
-            <li><a href="/boarding_passes.php">Boarding Passes</a></li>
-            <li><a href="/policies">Policies</a></li>
-            <!--blank for now-->
-            <li><a href="/login_register.php">User Profile</a></li>
-          </ul>
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <!-- this BAX Airlines header is also home/index/search flight -->
+          <a class="navbar-brand active" href="/#"><b>BAX Airlines</b></a>
         </div>
-      </nav>
-    </div>
+        <ul class="nav justify-content-end">
+          <li class="nav-item">
+            <a class="nav-link" href="#">Book a Flight</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/boarding_passes.php">Boarding Passes</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/profile.php">User Profile</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/login_register.php">
+              <?php echo $_SESSION['login'] ?>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </div>
     <div class= "boarding pass information">
       <h2>ATL to LAX</h2>
         <p>
@@ -45,15 +65,3 @@
     </div>
   </body>
 </html>
-
-<?php
-  include 'config/database.php';
-  include 'class/accounts.php';
-  include 'class/airports.php';
-  include 'class/flight_seats.php';
-  include 'class/flights.php';
-  include 'class/user_bookings.php';
-  require 'config/simple_html_dom.php';
-  $database = new Database();
-  $db = $database->getConnection();
-    ?>
